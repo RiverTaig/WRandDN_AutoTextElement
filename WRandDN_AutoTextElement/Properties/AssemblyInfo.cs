82f2b09c6a79c5c569abd8db1c5cddb3affafa60
RiverTaig <River.taig@Gmail.com>
2015-05-18 15:38:47 -0600

    relies on the caption to return WR-DN

---
+++ WRandDN_AutoTextElement/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WRandDN_AutoTextElement")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("WRandDN_AutoTextElement")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e5b53ae1-ff2c-4db3-837a-207ef3ac6177")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: log4net.Config.RepositoryAttribute("Miner")]
+#if DEBUG
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "miner.log4netD.config", Watch = true)]
+#else
+	[assembly: log4net.Config.XmlConfigurator(ConfigFile = "miner.log4net.config",Watch=true)]
+#endif
---
+++ WRandDN_AutoTextElement/WRandDN_AutoTextElement.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.esriSystem;
+
+using Miner.ComCategories;
+using Miner.Interop;
+namespace WRandDN_AutoTextElement
+{
+    [ComVisible(true)]
+    [Guid("4c73e554-8966-3333-b7bb-5ab425108d46")]
+    [ProgId("SE.WRandDN_AutoTextElement")]
+    [ComponentCategory(ComCategory.MMCustomTextSources)]
+    public class WRandDN_AutoTextElement : IMMAutoTextSource
+    {
+        private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        #region IMMAutoTextSource Members
+
+        public string Message
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public string ProgID
+        {
+            get
+            {
+                return "SE.WRandDN_AutoTextElement";
+            }
+        }
+
+        public string Caption
+        {
+            get
+            {
+                return "SE: Work Request Name - Design Name";
+            }
+        }
+
+        public bool NeedRefresh(Miner.Interop.mmAutoTextEvents eTextEvent)
+        {
+            return true;
+            if (eTextEvent == mmAutoTextEvents.mmCreate |
+                eTextEvent == mmAutoTextEvents.mmPlotNewPage)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string TextString(Miner.Interop.mmAutoTextEvents eTextEvent, IMMMapProductionInfo pMapProdInfo)
+        {
+            try
+            {
+                Type t2 = Type.GetTypeFromProgID("esriFramework.AppRef");
+                System.Object obj = Activator.CreateInstance(t2);
+                ESRI.ArcGIS.Framework.IApplication app = obj as ESRI.ArcGIS.Framework.IApplication;
+                return app.Caption.Substring(0,app.Caption.IndexOf("(") - 1);
+                /*switch (eTextEvent)
+                {
+                    
+                    case mmAutoTextEvents.mmCreate:
+                        {
+                            return this.Caption;
+                        }
+                    case mmAutoTextEvents.mmPlotNewPage:
+                        {
+
+                            return "xxx";
+                        }
+                    default:
+                        {
+                            return "";
+                        }
+                }*/
+            }
+            catch (Exception ex)
+            {
+                _log.Error("ERROR - ", ex);
+                return "";
+            }
+
+        }
+
+        #endregion
+    
+    }
+}
